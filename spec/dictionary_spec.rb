b41d79432cf3dccd056509bebf43ba16e723644c
Kerry Buckley <kerryjbuckley@gmail.com>
2008-04-02 13:21:56 +0100

    Spec for returning cached value

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -3,12 +3,21 @@ $: << File.dirname(__FILE__) + '/../lib'
 require 'dictionary'
 
 describe Dictionary, "looking up a value" do
+  before do
+    @store = mock 'store'
+    @cache = mock 'cache'
+    @key='foo'
+    @value = 'bar'
+    @cache.stub!(:get).and_return @value
+    @dictionary = Dictionary.new(@store, @cache)
+  end
+  
   it "should look in the cache" do
-    store = mock 'store'
-    cache = mock 'cache'
-    key='foo'
-    dictionary = Dictionary.new(store, cache)
-    cache.should_receive(:get).with(key)
-    dictionary.lookup key
+    @cache.should_receive(:get).with(@key)
+    @dictionary.lookup @key
+  end
+  
+  it "should return the value returned from the cache" do
+    @dictionary.lookup(@key).should == @value
   end
 end
