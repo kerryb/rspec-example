30b3ccd7138a82427c14d3c6b11aed1d9e67d49d
Kerry Buckley <kerryjbuckley@gmail.com>
2008-04-02 13:27:01 +0100

    Look in store too

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,9 +1,11 @@
 class Dictionary
   def initialize store, cache
     @cache = cache
+    @store = store
   end
   
   def lookup key
     @cache.get key
+    @store.get key
   end
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -9,15 +9,22 @@ describe Dictionary, "looking up a value" do
     @key='foo'
     @value = 'bar'
     @cache.stub!(:get).and_return @value
+    @store.stub!(:get).and_return @value
     @dictionary = Dictionary.new(@store, @cache)
   end
   
   it "should look in the cache" do
-    @cache.should_receive(:get).with(@key)
+    @cache.should_receive(:get).with @key
     @dictionary.lookup @key
   end
   
   it "should return the value returned from the cache" do
     @dictionary.lookup(@key).should == @value
+  end
+  
+  it "should look in the store if the value is not in the cache" do
+    @cache.stub!(:get).and_return nil
+    @store.should_receive(:get).with @key
+    @dictionary.lookup @key
   end
 end
